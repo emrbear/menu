c78c6cd752e59579aff61caa8da10916443c2e70
Andres Paglayan <andres@paglayan.com>
2020-07-31 16:29:09 -0600

    second session

--- app/controllers/cartes_controller.rb
+++ app/controllers/cartes_controller.rb
@@ -1,10 +1,43 @@
 class CartesController < ApplicationController
 
   def index
+    @cartes = Carte.by_position
   end
 
   def new
     @carte = Carte.new
+  end
+
+  def create
+    @carte = Carte.new( carte_params )
+    if @carte.save
+      redirect_to action: :index
+    else
+      render action: :new
+    end
+  end
+
+  def edit
+    @carte = Carte.find(params[:id])
+  end
+
+  def update
+    @carte = Carte.find(params[:id])
+    if @carte.update_attributes carte_params
+      redirect_to action: :index
+    else
+      render action: :edit
+    end
+  end
+
+  def destroy
+    @carte = Carte.find(params[:id])
+    @carte.destroy
+    redirect_to action: :index
+  end
+
+  def carte_params
+    params.require(:carte).permit(:title, :subtitle, :position)
   end
 
 end
--- app/models/carte.rb
+++ app/models/carte.rb
@@ -4,4 +4,5 @@ class Carte < ApplicationRecord
   validates :subtitle, presence: true
   validates :position, presence: true, numericality: true
 
+  scope :by_position, ->(){ order(:position) }
 end
